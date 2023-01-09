b9736b9da66f5ac280837b8db0da20be1d485f66
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2023-01-09 11:55:34 -0600

    contact page complete

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,28 @@
 import './index.css'
 
-function Contact () {
+function Contact() {
   return (
-    <div> Contact </div>
+    <div className="contact-page">
+      <h2>Get in Touch</h2>
+      <p>
+        I am currently looking for new opportunities in the Austin area.
+        My inbox is always open.
+      </p>
+      <p>
+        Feel free to reach out to me.
+        I look forwarded to connecting.
+      </p>
+      <a href="mailto:ericaemorabito@gmail.com"><button id="email-btn">Email</button></a>
+    </div>
+
+    // <div className="contact-page">
+    //   <form className="contact-form">
+    //     <label for="email">Email:</label>
+    //     <input type="text" name="email"></input><br></br>
+    //     <label for="message">Message:</label>
+    //     <input type="text" name="message"></input>
+    //   </form>
+    // </div>
   )
 }
 
